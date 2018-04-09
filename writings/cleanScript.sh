c5befbd0b18f6dc1543e0c50b07f2b6ac4bc28e5
Arne on MAC <arne.hansen@usi.ch>
2018-04-09 16:14:40 +0200

    add uncomment line to script

--- writings/cleanScript.sh
+++ writings/cleanScript.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-# comment out all lipsum
+# comment lipsum
 perl -p -i -e 's/(?<!%)(\\lipsum)/%\2/g' `find ./ -name "*.tex"`
+
+# uncomment lipsum
+# perl -p -i -e 's/%(\\lipsum)/\1/g' `find ./ -name "*.tex"`
