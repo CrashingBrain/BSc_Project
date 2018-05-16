8f2ab7545c3aa01f6e57f2c3b36b964d96b5479c
Arne on MAC <arne.hansen@usi.ch>
2018-05-16 13:13:06 +0200

    add functions

--- coding/info.py
+++ coding/info.py
@@ -1,10 +1,30 @@
 import numpy as np
 
+def coeffOfNo( no, mixedBasis):
+    coeffs = ()
+    for k in len(mixedBasis):
+        coeffs = coeffs + ( no//np.prod( mixedBasis(k:)))
+        no = no%np.prod( mixedBasis(k:))
+    return coeffs()
+        
 def randChannel( dim_out, dim_in):
     PC = np.random.rand(dim_out, dim_in)
     for i in range(0,dim_in):
         factor = 1./np.sum(PC[:,i])
         PC[:,i] = np.multiply( factor, PC[:,i])
+    return PC
+
+# Assume now that there are multiple parties for inputs and outputs
+# -> dim_out, dim_in are lists
+# (No easy way of fixing the last (dim_in) indeces to perform sum etc on the resulting array...)
+def randChannelMultipart( dim_out, dim_in):
+    PC = np.random.rand( dim_out+dim_in);
+    for k in np.prod(dim_in):
+        factor = 0.
+        for l in np.prod(dim_out):
+            factor += PC[coeffOfNo( l, dim_out) + coeffOfNo( k, dim_in)]
+        for l in np.prod(dim_out):
+            PC[coeffOfNo( l, dim_out) + coeffOfNo( k, dim_in)] *= 1./factor
     return PC
 
 # Seems to swap "channelled" party always to the very end
@@ -40,5 +60,11 @@ def MCupperBoundIntrinInf(P, noIter):
             minVal = val
     return minVal
             
-
-
+# Monte Carlo way of computing an upper bound on the reduced intrinsic information
+# -> need to choose 2 channels at random
+# -> choose size of U as product of 3 dimensions X,Y,Z
+def MCupperBoundRedIntrinInf( P, noIterOuter, noIterInner):
+    minVal = 0.
+    for i in range(0, noIterOuter):
+        PC_U_XYZ = randChannel( P.shape[2], P.shape[2])
+        for k in range(0, noIterInner):
