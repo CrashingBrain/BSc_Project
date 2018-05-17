eab5429c12c88efcfde9e929378eeb1e92f43409
Luca Dolfi <ldolfi@gmail.com>
2018-05-17 18:37:56 +0200

    Revert "add latest changes"
    This reverts commit a9bdce193a3b0a467e41b81f9d7675843360cbe3.

--- coding/test_info.py
+++ coding/test_info.py
@@ -1,21 +1,15 @@
-import bhvs as bv
+import prob as pr
 import info as inf
 import numpy as np
 
 PC = inf.randChannel(3,2)
-print(PC.shape)
 print(PC)
-
-P = bv.FourPDstrb()
+P = pr.FourPDstrb()
+print(PC.shape)
 print(P.shape)
 print(inf.applyChannel(P, PC, 2).shape)
 print(inf.applyChannel(P, PC, 3).shape)
 print(inf.applyChannel(P, PC, 2))
 print(inf.applyChannel(P, PC, 3))
-print(inf.mutInf(pr.marginal(P, (2,3))))
 
-# Loop over different random channels
-for k in 10:
-    PC = inf.randChannel(2,2)
-    print( inf.mutInf( pr.marginal( inf.applyChannel(P, PC, 3), (2,3))))
-    pass
+print(inf.mutInf(pr.marginal(P, (2,3))))
