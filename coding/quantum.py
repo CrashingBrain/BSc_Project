e778dca222c427926d4b555946df07a52b84ca1a
Arne <arne.hansen@usi.ch>
2018-05-18 22:36:36 +0200

    fix bugs

--- coding/quantum.py
+++ coding/quantum.py
@@ -1,40 +1,43 @@
 import numpy as np
 
+def pt(rho, dims):
+    # flatten rho to array
+    rho.shape = np.power(np.prod( dims),2)
+    # build matrix for PT
+    PT = np.zeros( (np.power(np.prod(dims),2), np.power(np.prod(dims),2)))
+    for m in range(0, dims[0]):
+        for n in range(0, dims[0]):
+            for k in range(0,dims[1]):
+                for l in range(0,dims[1]):
+                    PT[ m + dims[0] * (k + dims[1] * ( n + dims[0] * l)), m + dims[0] * (l + dims[1] * ( n + dims[0] * k))] = 1
+    rho_pt = np.matmul( PT, rho)
+    rho_pt.shape = ( np.prod(dims), np.prod(dims))
+    return rho_pt
+
 # expect rho to be 2 partite density matrix
 # return 1 if positive partial transpose
 def ppt(rho, dims):
-    # flatten rho to array
-    rho.shape = np.power(np.prod( dims),2)
-    T = np.zeros( np.power(dims[1],2), np.power(dims[1],2))
-    for k in range(0,dims[1]):
-        for l in range(0,dims[1]):
-            T[ k + dims[1]*l, l + dims[1]*k] = 1
-    PT_op = np.tensordot( np.identity( dims[0]), T)
-    rho_pt = np.matmul( PT_op, rho)
-    rho_pt.shape = ( np.prod(dims), np.prod(dims))
-    min_val = np.min( np.eig(rho_pt)).w )
+    min_val = np.linalg.eig( pt(rho, dims))[0].min()
+    print('qm.ppt.minval ' + str(min_val))
     if min_val > -1e-8:
         res = 1
     else:
-        res 0
+        res = 0
     return res
 
 # define bipartite rho from bipartite probability distribution
 def PrToRho(P):
     rho = np.zeros( np.prod( P.shape), np.prod( P.shape))
-    for i in P.shape[0]:
-        for j in P.shape[0]:
-            for k in P.shape[1]:
-                for l in P.shape[1]:
+    for i in range(0, P.shape[0]):
+        for j in range(0, P.shape[0]):
+            for k in range(0, P.shape[1]):
+                for l in range(0, P.shape[1]):
                     rho[ i + P.shape[0]*k, j + P.shape[0]*l] = np.sqrt( P[i,k]*P[j,l])
-    pass
+    return rho
 
 def proj(v):
-    if v.shape[1] > 1:
-        prj = np.matmul( v, np.transpose(v))
-    else:
-        prj = np.matmul( np.transpose(v), v)
-    return prj
+    v.shape = (len(v), 1)
+    return np.matmul( v, np.transpose(v))
 
 # Witness from quant-ph/0308032  (VII, B)
 # For all separable states: tr(W*rho) >=0
