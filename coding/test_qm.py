f652564e8134d92c1f860bb96de141d85da1a58c
Arne <arne.hansen@usi.ch>
2018-05-18 22:46:34 +0200

    add latest changes

--- coding/test_qm.py
+++ coding/test_qm.py
@@ -1,4 +1,5 @@
 import bhvs as bv
+import prob as pr
 import info as inf
 import numpy as np
 import quantum as qm
@@ -11,4 +12,7 @@ print( qm.pt(rho, (2,2)))
 print( qm.ppt(rho, (2,2)))
 
 # Transfer prob to state and check witness
+rho1 = qm.PrToRho( pr.marginal( bv.ThreePDstrb(), 2))
+print(rho1)
+print( np.trace( np.matmul( rho1, qm.wtns44())))
 # TO BE DONE
