aaba379c5b81c181abc0a1fa3e084e49f3ed3b6f
Arne <arne.hansen@usi.ch>
2018-05-18 22:36:36 +0200

    fix bugs

--- coding/test_qm.py
+++ coding/test_qm.py
@@ -4,9 +4,11 @@ import numpy as np
 import quantum as qm
 
 # Get some entangled vector
-psi = np.multiply( 1./np.sqrt(2), np.identity(4)[0, :]+np.identity(4)[3,:])
+psi = np.multiply( 1./np.sqrt(2), np.identity(4)[:, 0]+np.identity(4)[:,3])
 rho = qm.proj(psi)
-print( qm.ppt(rho))
+print( rho)
+print( qm.pt(rho, (2,2)))
+print( qm.ppt(rho, (2,2)))
 
 # Transfer prob to state and check witness
 # TO BE DONE
