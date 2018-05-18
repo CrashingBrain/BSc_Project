affa717403055b3d611cecfd16af9b54c985d1cb
Arne <arne.hansen@usi.ch>
2018-05-18 10:00:57 +0200

    add proper formatting

--- coding/prob.py
+++ coding/prob.py
@@ -16,16 +16,15 @@ def FourDimToTwo(P):
                     P_res[ x*P.shape[1] + y, z*P.shape[3] + u ] = P[x,y,z,u]
     return P_res
 
-# WANTED: Proper formatting a la fprintf
 def PrintFourPDstrb(P):
     s = str()
-    s += '( x, y) '
+    s += '(   x,   y) '
     for z in range(0, P.shape[2]):
         for u in range(0, P.shape[3]):
             s += '( '
-            s += str(z)
+            s += str('%3d' %z)
             s += ', '
-            s += str(u)
+            s += str('%3d' %u)
             s += ') '
             #s += '(' + str(z) + ', ' + str(u) ')'
     s += '   (mrg)'
@@ -34,26 +33,60 @@ def PrintFourPDstrb(P):
         for y in range(0, P.shape[1]):
             s = str()
             s += '( '
-            s += str(x)
+            s += str('%3d' %x)
             s += ', '
-            s += str(y)
+            s += str('%3d' %y)
             s += ') '
             #s += ('(' + str(x) + ', ' + str(y) ') ')
             for z in range(0, P.shape[2]):
                 for u in range(0, P.shape[3]):
                     if P[x,y,z,u] > 0:
-                        s += str(P[x,y,z,u])
+                        s += str('     %0.4f' %P[x,y,z,u])
                     else:
-                        s += '        '
+                        s += '           |'
             s += '   '
-            s += str(np.sum(P, (2,3))[x,y])
+            s += str(' %0.4f' %np.sum(P, (2,3))[x,y])
             print(s)
     s = str()
-    s += '(mrg) '
+    s += '( marginal) '
     for z in range(0, P.shape[2]):
         for u in range(0, P.shape[3]):
-            s += str(np.sum(P, (0,1))[z,u])
+            s += str('     %0.3f' %np.sum(P, (0,1))[z,u])
             s += ' ' 
+    print(s)
+    pass
+
+def PrintThreePDstrb(P):
+    s = str()
+    s += '(   x,   y) '
+    for z in range(0, P.shape[2]):
+            s += ' ( '
+            s += str('%3d' %z)
+            s += ')'
+    s += '   (mrg)'
+    print(s)
+    for x in range(0, P.shape[0]):
+        for y in range(0, P.shape[1]):
+            s = str()
+            s += '( '
+            s += str('%3d' %x)
+            s += ', '
+            s += str('%3d' %y)
+            s += ') '
+            #s += ('(' + str(x) + ', ' + str(y) ') ')
+            for z in range(0, P.shape[2]):
+                if P[x,y,z] > 0:
+                    s += str(' %0.4f' %P[x,y,z])
+                else:
+                    s += '      |'
+            s += '   '
+            s += str(' %0.4f' %np.sum(P, (2))[x,y])
+            print(s)
+    s = str()
+    s += '( marginal) '
+    for z in range(0, P.shape[2]):
+        s += str(' %0.3f' %np.sum(P, (0,1))[z])
+        s += ' ' 
     print(s)
     pass
 
