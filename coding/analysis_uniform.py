94a655153be8c9dc2cc4a58a4e5fc4ac5e7e6277
Luca Dolfi <ldolfi@gmail.com>
2018-05-21 03:36:19 +0200

    fix and rename test_uniform

--- coding/analysis_uniform.py
+++ coding/analysis_uniform.py
@@ -2,6 +2,7 @@ import prob as pr
 import bhvs as bv
 import info as inf
 import numpy as np
+import operator
 
 P = bv.ThreePDstrb()
 
@@ -25,13 +26,13 @@ intrIs = []
 redIntrIs = []
 print('getting values...')
 for i, p in enumerate(Ps):
-    m = pr.marginal(p, (2))
+    m = pr.marginal(p, (2,))
     # calculate mutual information of the marginal X,Y
     mutIs.append(inf.mutInf(m))
     # use MC to get upper bound to intrinsic info
     intrIs.append(inf.MCupperBoundIntrinInf(p, inIter))
     # use MC to get upper bound on reduced intrinsic info
-    redIntrIs.append(inf.MCupperBoundRedIntrinInf(p, inIter, outIter))
+    # redIntrIs.append(inf.MCupperBoundRedIntrinInf(p, inIter, outIter))
     print("done %d/%d" % (i, len(Ps)))
 # print results
 
@@ -43,4 +44,5 @@ print(intrIs); print()
 print('estimated upper bound to reduced intr. info:')
 print(redIntrIs); print()
 print('estiamted gap between the two:')
-print(intrIs - redIntrIs); print()
+diff = list(map(operator.sub, intrIs, redIntrIs))
+print(diff); print()
